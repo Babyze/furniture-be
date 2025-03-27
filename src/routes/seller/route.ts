3f757229511bcb203e016965d71c5b65156df850
Babyze <danghuynhanh1999@gmail.com>
2025-03-27 16:34:24 +0700

    Commit validate jwt middleware

--- src/routes/seller/route.ts
+++ src/routes/seller/route.ts
@@ -1,9 +1,18 @@
 import { SELLER_ROUTE_NAME } from '@src/constant/route-name.constant';
 import express from 'express';
 import authRoute from './auth.route';
+import { validateJwtMiddleware } from '@src/middlewares/validate-jwt.middleware';
+import { SellerJwtService } from '@src/services/seller/auth/jwt.service';
 
 const router = express.Router();
+const sellerJwtService = new SellerJwtService();
 
+// Public routes
 router.use(SELLER_ROUTE_NAME.AUTH.NAME, authRoute);
+
+// Protected routes
+router.use(validateJwtMiddleware(sellerJwtService));
+
+// Add protected routes here
 
 export default router;
